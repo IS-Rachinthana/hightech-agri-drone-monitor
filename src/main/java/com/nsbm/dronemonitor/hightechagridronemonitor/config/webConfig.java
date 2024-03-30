d1d4b93b1f3b40378d193463bb007e449ad9fd14
Savindu <isavindurachintha@gmail.com>
2024-03-30 12:45:03 +0530

    change

--- src/main/java/com/nsbm/dronemonitor/hightechagridronemonitor/config/webConfig.java
+++ src/main/java/com/nsbm/dronemonitor/hightechagridronemonitor/config/webConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
-//comments
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")

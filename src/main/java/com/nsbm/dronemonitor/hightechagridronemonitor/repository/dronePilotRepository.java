b1154101fdfe98c4ebaa301445b262cd83bbf4c9
Savindu <isavindurachintha@gmail.com>
2024-03-30 12:55:51 +0530

    chnage the file structure

--- src/main/java/com/nsbm/dronemonitor/hightechagridronemonitor/repository/dronePilotRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nsbm.dronemonitor.hightechagridronemonitor.repository;
-
-
-import com.nsbm.dronemonitor.hightechagridronemonitor.model.dronePilot;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface dronePilotRepository extends JpaRepository<dronePilot, Integer> {
-}

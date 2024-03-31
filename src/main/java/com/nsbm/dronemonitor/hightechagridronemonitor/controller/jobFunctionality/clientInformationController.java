1be2c0f3e63ff86b32ab9bdb230ec97ba8e3cd0c
Savindu <isavindurachintha@gmail.com>
2024-03-31 07:35:35 +0530

    change the class

--- src/main/java/com/nsbm/dronemonitor/hightechagridronemonitor/controller/jobFunctionality/clientInformationController.java
+++ src/main/java/com/nsbm/dronemonitor/hightechagridronemonitor/controller/jobFunctionality/clientInformationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class ClientInformationController {
+public class clientInformationController {
     @Autowired
     //ClientInformationService clientInformationService;
     private ClientInformationRepository clientInformationRepository;
